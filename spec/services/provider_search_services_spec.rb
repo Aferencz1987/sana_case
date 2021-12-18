86c17aa9497483b2c397ee26334073a30e8af802
Alex Ferencz <alex.ferencz6@gmail.com>
2021-12-18 16:13:42 -0600

    Search services with tests

--- spec/services/provider_search_services_spec.rb
+++ spec/services/provider_search_services_spec.rb
@@ -11,14 +11,33 @@ RSpec.describe 'NPPES NPI registry api' do
         expect(result[:results][0]).to have_key(:addresses)
         expect(result[:results][0][:basic][:last_name]).to eq("KEMP")
         expect(result[:results][1][:basic][:last_name]).to eq("KEMP")
-        expect(result[:results][1][:basic][:last_name]).to_not eq("BART")
+        expect(result[:results][2][:basic][:last_name]).to_not eq("BART")
       end
     end
 
     it 'returns information based on last name and first name' do
       VCR.use_cassette 'kemp_laura' do
         result = ProviderSearchService.first_last_name('laura', 'kemp')
-require "pry"; binding.pry
+
+        expect(result[:results][0]).to have_key(:basic)
+        expect(result[:results][0]).to have_key(:number)
+        expect(result[:results][0]).to have_key(:addresses)
+        expect(result[:results][0][:basic][:last_name]).to eq("KEMP")
+        expect(result[:results][1][:basic][:last_name]).to eq("KEMP")
+        expect(result[:results][2][:basic][:last_name]).to_not eq("BART")
+      end
+    end
+
+    it 'returns information based on last name and first name' do
+      VCR.use_cassette 'kemp_midwife' do
+        result = ProviderSearchService.last_name_specialty('kemp', 'midwife')
+
+        expect(result[:results][0][:basic][:last_name]).to eq("KEMP")
+        expect(result[:results][1][:basic][:last_name]).to eq("KEMP")
+        expect(result[:results][2][:basic][:last_name]).to_not eq("BART")
+        expect(result[:results][0][:taxonomies][0][:desc]).to eq("Advanced Practice Midwife")
+        expect(result[:results][1][:taxonomies][0][:desc]).to eq("Advanced Practice Midwife")
+        expect(result[:results][2][:taxonomies][0][:desc]).to eq("Advanced Practice Midwife")
       end
     end
   end
