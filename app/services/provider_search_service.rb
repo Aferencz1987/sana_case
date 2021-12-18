2b138ba72ef9f0b19542b6f20d4afcb44c3e80ec
Alex Ferencz <alex.ferencz6@gmail.com>
2021-12-18 15:09:01 -0600

    Last name and location search with test

--- app/services/provider_search_service.rb
+++ app/services/provider_search_service.rb
@@ -1,18 +1,20 @@
 class ProviderSearchService
 
   def self.last_name_location(last_name, state)
-    response = conn.get "?number=&enumeration_type=&taxonomy_description=&first_name=&use_first_name_alias=&last_name=#{last_name}&organization_name=&address_purpose=&city=&#{state}=&postal_code=&country_code=&limit=&skip=&version=2.1"
+    response = conn.get "?last_name=#{last_name}&state=#{state}&version=2.1"
       search_result = JSON.parse(response.body, symbolize_names: true)
   end
 
   def self.first_last_name(first_name, last_name)
-    response = conn.get "/?number=&enumeration_type=&taxonomy_description=&first_name=#{first_name}&use_first_name_alias=&last_name=#{last_name}&organization_name=&address_purpose=&city=&state=&postal_code=&country_code=&limit=&skip=&version=2.1"
+    response = conn.get "?first_name=#{first_name}&last_name=#{last_name}&version=2.1"
       search_result = JSON.parse(response.body, symbolize_names: true)
   end
+
   def self.last_name_specialty(last_name, specialty)
-    response = conn.get "/?number=&enumeration_type=&taxonomy_description=#{specialty}&first_name=&use_first_name_alias=&last_name=#{last_name}&organization_name=&address_purpose=&city=&state=&postal_code=&country_code=&limit=&skip=&version=2.1"
+    response = conn.get "?taxonomy_description=#{specialty}&last_name=#{last_name}&version=2.1"
       search_result = JSON.parse(response.body, symbolize_names: true)
   end
+
   def self.conn
     Faraday.new(url: 'https://npiregistry.cms.hhs.gov/api/') do |faraday|
     end
--- spec/services/provider_search_services_spec.rb
+++ spec/services/provider_search_services_spec.rb
@@ -1,17 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'NPPES NPI registry api' do
-  it 'returns provider information' do
-    VCR.use_cassette 'kemp' do
-      result = ProviderSearchService.last_name_location('kemp', 'co')
+  describe 'returns provider information' do
+    it 'returns information based on last name and location' do
+      VCR.use_cassette 'kemp_location' do
+        result = ProviderSearchService.last_name_location("kemp", "co")
+
+        expect(result[:results][0]).to have_key(:basic)
+        expect(result[:results][0]).to have_key(:number)
+        expect(result[:results][0]).to have_key(:addresses)
+        expect(result[:results][0][:basic][:last_name]).to eq("KEMP")
+        expect(result[:results][1][:basic][:last_name]).to eq("KEMP")
+        expect(result[:results][1][:basic][:last_name]).to_not eq("BART")
+      end
+    end
+
+    it 'returns information based on last name and first name' do
+      VCR.use_cassette 'kemp_laura' do
+        result = ProviderSearchService.first_last_name('laura', 'kemp')
 require "pry"; binding.pry
-      expect(result).to have_key(:lon)
-      expect(result).to have_key(:lat)
-      expect(result).to_not have_key(:minutely)
-      expect(result[:current]).to have_key(:temp)
-      expect(result[:hourly].first).to have_key(:temp)
-      expect(result[:daily].first).to have_key(:temp)
-      expect(result[:current][:temp]).to eq(90.81)
+      end
     end
   end
 end
