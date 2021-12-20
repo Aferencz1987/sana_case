c0916fe489c2818222574a072f6d563501aa7ccb
Alex Ferencz <alex.ferencz6@gmail.com>
2021-12-20 10:53:23 -0700

    Provider poro npi number issue

--- app/facades/provider_search_facade.rb
+++ app/facades/provider_search_facade.rb
@@ -1,28 +1,67 @@
 class ProviderSearchFacade
   def self.last_name_location(last_name, state)
     provider_response = ProviderSearchService.last_name_location(last_name, state)
-    result = provider_response[:results]
-
-    result.map do |provider_info|
-      ProviderPoro.new(provider_info)
+    provider_response[:results].map do |provider_info|
+      attributes =
+      {
+        first_name: provider_info[:basic][:first_name],
+        last_name: provider_info[:basic][:last_name],
+        npi_number: provider_info[:number],
+        specialty: provider_info[:taxonomies][0][:desc],
+        address_list: provider_info[:addresses].map do |addresses|
+          {
+            address_1: addresses[:address_1],
+            address_2: addresses[:address_2],
+            city: addresses[:city],
+            state: addresses[:state]
+          }
+        end
+      }
+      ProviderPoro.new(attributes)
     end
   end
 
   def  self.first_name_last_name(first_name, last_name)
     provider_response = ProviderSearchService.first_last_name(first_name, last_name)
-    result = provider_response[:results]
-
-    result.map do |provider_info|
-      ProviderPoro.new(provider_info)
+    provider_response[:results].map do |provider_info|
+      attributes =
+      {
+        first_name: provider_info[:basic][:first_name],
+        last_name: provider_info[:basic][:last_name],
+        npi_number: provider_info[:number],
+        specialty: provider_info[:taxonomies][0][:desc],
+        address_list: provider_info[:addresses].map do |addresses|
+          {
+            address_1: addresses[:address_1],
+            address_2: addresses[:address_2],
+            city: addresses[:city],
+            state: addresses[:state]
+          }
+        end
+      }
+      ProviderPoro.new(attributes)
     end
   end
 
   def self. last_name_specialty(last_name, specialty)
      provider_response = ProviderSearchService.last_name_specialty(last_name, specialty)
-     result = provider_response[:results]
-
-     result.map do |provider_info|
-       ProviderPoro.new(provider_info)
+     provider_response[:results].map do |provider_info|
+       attributes =
+       {
+         first_name: provider_info[:basic][:first_name],
+         last_name: provider_info[:basic][:last_name],
+         npi_number: provider_info[:number],
+         specialty: provider_info[:taxonomies][0][:desc],
+         address_list: provider_info[:addresses].map do |addresses|
+           {
+             address_1: addresses[:address_1],
+             address_2: addresses[:address_2],
+             city: addresses[:city],
+             state: addresses[:state]
+           }
+         end
+       }
+       ProviderPoro.new(attributes)
      end
   end
 end
--- spec/poros/provider_poro_spec.rb
+++ spec/poros/provider_poro_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Provider poro' do
-  it 'has attributes of a provider' do
+   it 'has attributes of a provider' do
     attributes = {first_name: "first name test",
                   last_name: "last name test",
-                  npi_number: "1234567",
+                  npi_number: 1234567,
                   specialty: "doctoring",
-                  address_list: "!!!!FIGURE ME OUT!!!"
+                  address_list: {address_1: "7720 S BROADWAY", address_2: "STE 590", city: "LITTLETON", state: "CO"}
                  }
 
     provider = ProviderPoro.new(attributes)
-    
+
     expect(provider.first_name).to eq(attributes[:first_name])
     expect(provider.specialty).to be_a String
-    # expect(provider.address_list).to be_a Array
+    require "pry"; binding.pry
   end
 end
